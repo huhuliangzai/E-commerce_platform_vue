b62dc3e3280040198c33ef8cb61db1be5ae379d0
liangzai <1821995845@qq.com>
2023-05-21 15:21:57 +0800

    modified:   src/views/MyOrdersView/MyOrderView.vue 	new file:   src/views/MyOrdersView/OrderList/AllOrder.vue 	new file:   src/views/MyOrdersView/OrderList/BeEvaluated.vue 	new file:   src/views/MyOrdersView/OrderList/ToBeReceived.vue

--- src/views/MyOrdersView/MyOrderView.vue
+++ src/views/MyOrdersView/MyOrderView.vue
@@ -7,9 +7,15 @@
             </div>
             <div class="row border m-0 p-0 mt-3 all_orders">
                 <el-tabs v-model="activeName" class="demo-tabs">
-                    <el-tab-pane label="全部订单" name="first">User</el-tab-pane>
-                    <el-tab-pane label="待收货/使用" name="second">Config</el-tab-pane>
-                    <el-tab-pane label="待评价" name="third">Role</el-tab-pane>
+                    <el-tab-pane label="全部订单" name="first">
+                        <allorder/>
+                    </el-tab-pane>
+                    <el-tab-pane label="待收货/使用" name="second">
+                        <ToBeReceived/>
+                    </el-tab-pane>
+                    <el-tab-pane label="待评价" name="third">
+                        <BeEvaluated/>
+                    </el-tab-pane>
                   </el-tabs>
             </div>
         </div>
@@ -21,6 +27,9 @@ import { RouterLink,RouterView } from "vue-router";
 import { ref } from 'vue'
 
 import navBar from '@/components/header.vue'
+import allorder from './OrderList/AllOrder.vue'
+import ToBeReceived from "./OrderList/ToBeReceived.vue";
+import BeEvaluated from "./OrderList/BeEvaluated.vue";
 
 const activeName = ref('first')
 </script>
@@ -36,10 +45,6 @@ const activeName = ref('first')
 .demo-tabs > .el-tabs__content {
     color: #6b778c;
     font-weight: 600;
+    
 }
-
-.el-tabs__item.is-active{
-    color: red;
-}
-
 </style>
---
+++ src/views/MyOrdersView/OrderList/ToBeReceived.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="row p-0 m-0">
+        <div class="card p-0 m-0">
+            <div class="card-header p-0 m-0 py-2 d-flex justify-content-end">
+                <ul class="d-flex p-0 m-0 justify-content-around" style="width: 65%;">
+                    <li>订单详情</li>
+                    <li>收货人</li>
+                    <li>金额</li>
+                    <li>订单状态</li>
+                </ul>
+            </div>
+            <div class="d-flex card-body p-0 m-0 mb-2">
+                <div class="left d-flex justify-content-center align-items-center">
+                    <img class="img-fluid" src="@/assets/image/good_show_img/6.jpg" alt="">
+                    <h4 class="ms-2">
+                        <span>冷冻雪花肥牛</span><br>
+                        <span>300g</span>  
+                    </h4>
+                    
+                </div>
+                <div class="right ms-2 d-flex justify-content-around align-items-center text-center rounded">
+                    <div class="order_details">
+                        <p class="m-0 p-0">
+                            <small>
+                                <strong>配送至:</strong>
+                                <span>广东省中山市博爱七路25号</span>
+                            </small>
+                        </p>
+                    </div>
+                    <div class="order_Receiver">
+                        <span>蔡老六</span>
+                    </div>
+                    <div class="order_amount">
+                        <span>￥99</span>
+                    </div>
+                    <div class="order_status">
+                        <button class="btn btn-sm btn-outline-primary">已收货</button>
+                        <button class="btn btn-sm btn-outline-primary ms-1">拒收</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+.left{
+    width: 35%;
+}
+
+.left img{
+    width: 100px;
+    height: 100px;
+}
+
+.right{
+    width: 65%;
+    background-color: rgb(194, 249, 230);
+}
+
+.right div{
+    width: 25%;
+}
+</style>
